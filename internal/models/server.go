f9212f2ed1b14c875c80b384f6bc4b135fc9244a
Maksim Minaev <m.minaev@f6.ru>
2025-11-23 11:53:23 +0300

    added new order model

--- internal/models/server.go
+++ internal/models/server.go
@@ -1,7 +1,6 @@
 package models
 
 type NewOrderData struct {
-	UserID  string   `json:"userId" validate:"required"`
-	Points  []string `json:"orderPoints" validate:"required"`
-	Comment string   `json:"comment"`
+	UserID string `db:"customer_id" json:"userId" validate:"required"`
+	Points int    `db:"amount" json:"amount" validate:"required"`
 }
